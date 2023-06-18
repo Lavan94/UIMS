681a839c3c4d2f1eb7a1cf958c2993a9f1f5393c
Silviu Adrian Mitroi <58517491+Lavan94@users.noreply.github.com>
2023-06-18 14:34:41 +0300

    Make logout work in a small manner

--- zone-manager/zm-client/src/app/navbar/navbar.component.ts
+++ zone-manager/zm-client/src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ORGANIZATION_MANAGER_PAGE, OWNER_MANAGER_PAGE} from "../app-routing.module";
+import {HOME_LOGIN_PAGE, ORGANIZATION_MANAGER_PAGE, OWNER_MANAGER_PAGE} from "../app-routing.module";
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +10,7 @@ export class NavbarComponent {
   showFiller = false;
   zoneManagerLink: string = `/${ORGANIZATION_MANAGER_PAGE}`;
   ownerManagerLink: string = `/${OWNER_MANAGER_PAGE}`;
+  homeLoginLink: string = `/${HOME_LOGIN_PAGE}`;
   page: string = 'Organizations';
   loggedIn: boolean = true;
 }
